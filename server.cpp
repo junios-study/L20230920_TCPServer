f03808e50aa7eff6ac1a3714f852a59610d19a7c
junios <junios@naver.com>
2023-09-20 15:00:28 +0900

    네트워크 오더

--- server.cpp
+++ server.cpp
@@ -6,6 +6,21 @@
 
 using namespace std;
 
+//int main()
+//{
+//	WSAData wsaData;
+//	WSAStartup(MAKEWORD(2, 2), &wsaData);
+//
+//	int HostNumber = 0x12345678;
+//	printf("%x\n", HostNumber);
+//	printf("%x\n", htonl(HostNumber));
+//	printf("%x\n", ntohl(htonl(HostNumber)));
+//
+//	WSACleanup();
+//	return 0;
+//}
+
+
 int main()
 {
 	WSAData wsaData;
@@ -23,6 +38,8 @@ int main()
 	bind(ListenSocket, (SOCKADDR*)&ListenSock, sizeof(ListenSock));
 	listen(ListenSocket, SOMAXCONN);
 
+	srand((unsigned int)time(nullptr));
+
 	while (true)
 	{
 		SOCKADDR_IN ClientSock;
@@ -31,28 +48,33 @@ int main()
 
 		SOCKET ClientSocket = accept(ListenSocket, (SOCKADDR*)&ClientSock, &ClientSockLength);
 
-		srand((unsigned int)time(nullptr));
-
 		char Message[1024] = { 0, };
 		char Operators[] = { '+', '-', '/', '*', '%' };
 
 		int First = (rand() % 10000) + 1;
 		int Second = (rand() % 10000) + 1;
-		
-		memcpy(Message, &First, sizeof(First));
-		memcpy(Message+5, &Second, sizeof(Second));
+
+		int NetworkFirst = htonl(First);
+		int NetworkSecond = htonl(Second);
+
+		memcpy(Message, &NetworkFirst, sizeof(NetworkFirst));
+		memcpy(Message+5, &NetworkSecond, sizeof(NetworkSecond));
 		Message[4] = Operators[rand() % 5];
 
 		int SendByte = send(ClientSocket, Message, 9, 0);
 
 		char Result[1024] = { 0, };
 		int RecvByte = recv(ClientSocket, Result, 4, MSG_WAITALL);
+		int ResultNumber = 0;
+		memcpy(&ResultNumber, Result, 4);
+		ResultNumber = ntohl(ResultNumber);
 
 		cout << First;
 		cout << Message[4];
 		cout << Second;
 		cout << "=";
-		cout << *(int*)&Result << endl;
+		//cout << *(int*)(&(Result+4)) << endl;
+		cout << ResultNumber << endl;
 
 		closesocket(ClientSocket);
 	}
